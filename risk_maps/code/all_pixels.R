e5789508267d627f391dc6b715da5ae9630b20b6
anna-boser <46419226+anna-boser@users.noreply.github.com>
2021-03-30 08:47:20 -0700

    organized

--- risk_maps/code/all_pixels.R
+++ risk_maps/code/all_pixels.R
@@ -4,7 +4,6 @@ library(sf)
 library(lubridate)
 library(dplyr)
 library(stringr)
-# library(data.table)
 
 landcover_sf <- st_read(here::here("risk_maps", 
                                    "data", 
@@ -80,3 +79,60 @@ saveRDS(all_pixels, here::here("risk_maps",
                                "data", 
                                "processed_data", 
                                "all_pixels.RData"))
+
+
+library(data.table)
+
+day_files_11_16 <- list.files(here::here("risk_maps",
+                                          "data", 
+                                          "processed_data", 
+                                          "ECOSTRESS", 
+                                          "air_temperature"))
+day_files_11_16 <- day_files_11_16[as.integer(substr(day_files_11_16, 5, 6)) %in% 11:15] # the hour is 11 to 15 so this goes until 16
+
+
+pixelify <- function(file){
+  
+  date <- substr(file, 14, 23)
+  hhmmss <- str_extract(file, regex('[0-9]{2}:{1}[0-9]{2}:{1}[0-9]{2}'))
+  Year <- lubridate::year(ymd(date))
+  
+  landcover_shp <- filter(landcover_sf, year == Year | is.na(year))
+  
+  airtemp_raster <- raster(here::here("risk_maps",
+                                      "data", 
+                                      "processed_data", 
+                                      "ECOSTRESS", 
+                                      "air_temperature", 
+                                      file))
+  
+  all_pixels <- data.frame(date = date, 
+                           hhmmss = hhmmss, 
+                           landcover = "Other", 
+                           air_temperature = raster::values(airtemp_raster))
+  all_pixels$biting_rate <- bite(all_pixels$air_temperature)
+  all_pixels$transmission_prob <- transmit(all_pixels$air_temperature)
+  
+  landcovers_of_interest <- c("Urban", "Vegetable", "Orchard", "Fruit", "Field Crop", "Uncultivated")
+  
+  for (lc in landcovers_of_interest){
+    all_pixels$landcover <- ifelse(is.na(raster::values(raster::mask(airtemp_raster, 
+                                                                     filter(landcover_shp, landcover == lc)))), 
+                                   all_pixels$landcover, 
+                                   lc)
+  }
+  
+  all_pixels$Urban <- ifelse(is.na(raster::values(raster::mask(airtemp_raster, 
+                                                               filter(landcover_shp, landcover == "Urban")))), 
+                             FALSE, 
+                             TRUE)
+  
+  return (all_pixels)
+}
+
+day_pixels <- rbindlist(lapply(day_files_11_16, pixelify))
+
+saveRDS(day_pixels, here::here("risk_maps",
+                               "data", 
+                               "processed_data", 
+                               "day_pixels.RData"))
